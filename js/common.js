0507a2d85ef8bdb8ad5839459d2126dbaf0f2329
yeo sugyeong <ssyeo1204@gmail.com>
2023-06-21 15:22:27 +0900

    common js 작업

---
+++ js/common.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelectorAll('.menu li a')
+const menuItem = document.querySelectorAll('article')
+
+for(let i = 0; i < menuBtn.length; i++){
+  menuBtn[i].addEventListener('click', function(e){
+    e.preventDefault();
+    for(let j = 0; j < menuBtn.length; j++){
+      menuBtn[j].parentNode.classList.remove('active')
+      menuItem[j].style.display = 'none'
+    }
+    menuBtn[i].parentNode.classList.add('active')
+    targetID = this.getAttribute('href');
+    document.querySelector(targetID).style.display = 'block'
+  })
+}
