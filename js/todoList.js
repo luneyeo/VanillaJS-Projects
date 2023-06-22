b947e9770af2515eae07b299bdb03d207c540de4
yeo sugyeong <ssyeo1204@gmail.com>
2023-06-22 16:46:44 +0900

    storage 작업

--- js/todoList.js
+++ js/todoList.js
@@ -5,8 +5,22 @@ const listItems = document.querySelector('.list-items')
 const allDeleteBtn = document.querySelector('.all-delete')
 
 
-function submitValue(){
+const TODOS_KEY = "todos"
+// 스토리지에 저장
+const todos = []
+function saveTodos(){
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos))
+}
 
+function deleteTodo(){
+  this.parentNode.remove()
+}
+
+function offToggle(){
+  this.firstElementChild.classList.toggle('off')
+}
+
+function addTodo(){
   // 할일 리스트
   const listItem = document.createElement('li')
   listItem.classList.add('list-item')
@@ -15,45 +29,54 @@ function submitValue(){
   const inputValue = document.createElement('span')
   inputValue.classList.add('input-value')
   inputValue.textContent = inputBox.value
+  todos.push(inputBox.value)
 
   // 삭제 버튼
   const deleteBtn = document.createElement('span')
   deleteBtn.classList.add('material-symbols-outlined')
   deleteBtn.textContent = 'close'
-  
-  if(!inputBox.value){
-    // alert('내용을 입력하세요')
-  }
-  else {
-    listItem.appendChild(inputValue)
-    listItem.appendChild(deleteBtn)
-    listItems.appendChild(listItem)
 
-    inputBox.value = '';
-    inputBox.focus()
-  }
+  listItem.appendChild(inputValue)
+  listItem.appendChild(deleteBtn)
+  listItems.appendChild(listItem)
 
-  // 리스트 클릭시 취소선
-  listItem.addEventListener('click', function(){
-    inputValue.classList.toggle('off')
-  })
-
-  // 삭제 버튼 클릭시 사라짐
-  deleteBtn.addEventListener('click', function(){
-    this.parentNode.remove()
-  })
-
-  // 전체 삭제 버튼 클릭시
-  allDeleteBtn.addEventListener('click', function(){
+  deleteBtn.addEventListener('click', deleteTodo) // 버튼 클릭시 삭제
+  listItem.addEventListener('click', offToggle) // 리스트 클릭시 취소선
+  allDeleteBtn.addEventListener('click', function(){ // 전체 삭제 버튼 클릭시 전체 삭제
     listItem.remove()
   })
 }
 
-addBtn.addEventListener('click', submitValue)
-inputBox.addEventListener('keyup', function(e){
-  if(e.keyCode === 13){
-    submitValue()
+// 입력창 초기화, 자동 포커스
+function resetInputValue(){
+  inputBox.value = '';
+  inputBox.focus()
+  saveTodos()
+}
+
+// 제출
+function submitValue(){
+  if (!inputBox.value){
+
+  } else{
+    addTodo()
+    resetInputValue()
   }
-})
+}
 
+// 엔터 눌러서 제출
+function enterSubmitValue(e){
+  if(!inputBox.value){
 
+  } else if (e.keyCode === 13) {
+    addTodo()
+    resetInputValue()
+  }
+}
+
+addBtn.addEventListener('click', submitValue)
+inputBox.addEventListener('keyup', enterSubmitValue)
+
+const savedTodos = localStorage.getItem(TODOS_KEY)
+
+resetInputValue()
